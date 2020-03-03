973cf72617efb8731d23cfb3fb5f8211a69393a9
Denis Korotevich <volk.denis98@gmail.com>
2020-03-04 02:45:20 +0300

    feat: solve the problem

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,31 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    let resultArr = [];
+    if (arr.constructor != Array) throw Error('Error');
+    arr.forEach((el, i) => {
+        if (el == '--discard-next') {
+            //------skip add next element
+        } else
+            if (el == '--discard-prev') {
+                if (i > 0) {
+                    resultArr.pop();
+                }
+            } else
+                if (el == '--double-next') {
+                    if (arr.length - 1 > i) {
+                        resultArr.push(arr[i + 1]);
+                    }
+                } else {
+                    if (el == '--double-prev') {
+                        if (i > 0) {
+                            resultArr.push(arr[i - 1]);
+                        }
+                    }
+                    else {
+                        if (arr[i - 1] != '--discard-next') {
+                            resultArr.push(el);
+                        }
+                    }
+                }
+    });
+    return resultArr;
 };
